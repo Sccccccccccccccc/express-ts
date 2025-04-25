85c19b248f2b72baa249b6c861f8d05257ecda68
zzj <www1121c@126.com>
2025-04-25 17:22:37 +0800

    feat: 首页路由路径修改

--- routes/index.ts
+++ routes/index.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 
 
 /* GET home page. */
-router.get('/', indexController.index);
+router.get('/index', indexController.index);
 router.get('/randomTitle', indexController.randomTitle);
 
 export default router;
